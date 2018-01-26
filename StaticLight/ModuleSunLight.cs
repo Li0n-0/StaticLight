10f31279b24e27c6ee8742e457755c7535617534
Li0n-0 <selekta.lion@live.fr>
2018-01-26 21:04:27 +0100

    delay raycast

--- StaticLight/ModuleSunLight.cs
+++ StaticLight/ModuleSunLight.cs
@@ -10,6 +10,8 @@ namespace StaticLight
 	{
 		public string animationName;
 		public bool reverseAnimation = false;
+		public float delayLowTimeWrap = 2f;
+		public float delayHighTimeWrap = .1f;
 
 		private Animation animationComponent;
 		private bool animIsPlaying = false;
@@ -17,10 +19,14 @@ namespace StaticLight
 		private float animationSpeed;
 
 		private CelestialBody sun;
-//		private bool inSunLight = false;
+		private bool inSunLight = false;
 		private bool lightIsOn = false;
 
 		private bool hasStarted = false;
+
+		private List<WaitForSeconds> timeWrapDelays;
+
+		private bool mainCoroutineHasStarted = false;
 
 		void Start ()
 		{
@@ -42,9 +48,22 @@ namespace StaticLight
 				if (field.Name == "reverseAnimation") {
 					reverseAnimation = (bool)field.GetValue (this);
 				}
+				if (field.Name == "delayLowTimeWrap") {
+					delayLowTimeWrap = (float)field.GetValue (this);
+				}
+				if (field.Name == "delayHighTimeWrap") {
+					delayHighTimeWrap = (float)field.GetValue (this);
+				}
 			}
 			Debug.Log ("[StaticLight] animationName : " + animationName);
 			Debug.Log ("[StaticLight] reverseAnimaion : " + reverseAnimation);
+
+			timeWrapDelays = new List<WaitForSeconds> ();
+			timeWrapDelays.Add (new WaitForSeconds (delayHighTimeWrap));
+			timeWrapDelays.Add (new WaitForSeconds (delayLowTimeWrap));
+			timeWrapDelays.Add (new WaitForSeconds (delayLowTimeWrap / 2f));
+			timeWrapDelays.Add (new WaitForSeconds (delayLowTimeWrap / 3f));
+			timeWrapDelays.Add (new WaitForSeconds (delayLowTimeWrap / 4f));
 
 
 			foreach (Animation anim in gameObject.GetComponentsInChildren<Animation> ()) {
@@ -79,16 +98,60 @@ namespace StaticLight
 //			if (!hasStarted) {
 //				DoStart ();
 //			}
-			if (hasStarted) {
+			mainCoroutineHasStarted = false;
+			animIsPlaying = false;
+			if (this.isActiveAndEnabled && hasStarted) {
 				StopAllCoroutines ();
+
 				animationComponent.Stop (animationName);
 				animationComponent [animationName].time = 0;
 				animationComponent [animationName].speed = animationSpeed;
 				lightIsOn = false;
 				animIsPlaying = false;
+				StartCoroutine ("SearchTheSun");
 			}
 
 
+		}
+
+		private IEnumerator SearchTheSun ()
+		{
+			Debug.Log ("[StaticLight] SearchTheSun started");
+			mainCoroutineHasStarted = true;
+			while (true) {
+				
+				CheckSunPos ();
+				if (TimeWarp.CurrentRate < 5f) {
+					yield return timeWrapDelays [(int)TimeWarp.CurrentRate];
+				} else {
+					yield return timeWrapDelays [0];
+				}
+			}
+		}
+
+		private void CheckSunPos ()
+		{
+			
+
+			if (animIsPlaying) {
+				Debug.Log ("[StaticLight] CheckSunPos : Anim already playing");
+				return;
+			}
+
+			inSunLight = IsUnderTheSun ();
+			Debug.Log ("[StaticLight] CheckSunPos is : " + inSunLight);
+
+			if (inSunLight && lightIsOn) {
+				Debug.Log ("[StaticLight] CheckSunPos : should turn lights off");
+				StartCoroutine ("LightsOff");
+				return;
+			}
+			if (!inSunLight && !lightIsOn) {
+				Debug.Log ("[StaticLight] CheckSunPos : should turn lights on");
+				StartCoroutine ("LightsOn");
+				return;
+			}
+			Debug.Log ("[StaticLight] CheckSunPos : nothing wrong with the light, they are : " + lightIsOn);
 		}
 
 		void Update ()
@@ -97,14 +160,19 @@ namespace StaticLight
 			if (!hasStarted) {
 				return;
 			}
+
+			if (hasStarted && !mainCoroutineHasStarted) {
+				StartCoroutine ("SearchTheSun");
+			}
+
 			if (!animIsPlaying) {
-				if (IsUnderTheSun () && lightIsOn) {
-					StartCoroutine ("LightsOff");
-					return;
-				} else if (!IsUnderTheSun () && !lightIsOn) {
-					StartCoroutine ("LightsOn");
-					return;
-				}
+//				if (IsUnderTheSun () && lightIsOn) {
+//					StartCoroutine ("LightsOff");
+//					return;
+//				} else if (!IsUnderTheSun () && !lightIsOn) {
+//					StartCoroutine ("LightsOn");
+//					return;
+//				}
 				if (Input.GetKeyDown (KeyCode.X)) {
 					Debug.Log ("[StaticLight] click lightsOn");
 					StartCoroutine ("LightsOn");
@@ -124,9 +192,10 @@ namespace StaticLight
 //			Debug.Log ("[StaticLight] RAYCAST !");
 			RaycastHit hit;
 
-			if (Physics.Raycast (transform.position, sun.position, out hit, Mathf.Infinity, (1 << 10/* | 1 << 15*/))) {
+			if (Physics.Raycast (transform.position, sun.position, out hit, Mathf.Infinity, (1 << 10 | 1 << 15/*  | 1 << 28*/))) {
+				Debug.Log ("[StaticLight] hit is named : " + hit.transform.name);
 				if (hit.transform.name == sun.bodyName) {
-//					Debug.Log ("[StaticLight] found the sun");
+					
 					return true;
 				}
 			}
